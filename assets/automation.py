ce529e18b407d9de87905d51e23a584791782ecc
Lauren <lauren.main28@gmail.com>
2023-05-22 22:08:29 -0700

    adds error handling, rainbow menu styling, and updates readme

--- assets/automation.py
+++ assets/automation.py
@@ -1,6 +1,6 @@
 from rich.console import Console
 from rich.prompt import Prompt
-# from rich.table import Table
+from rich.text import Text
 import shutil
 import re
 import os
@@ -9,9 +9,9 @@ import os
 def create_folder(folder_name):
     try:
         os.makedirs(folder_name)
-        print(f"folder '{folder_name}' created successfully.")
+        console.print(f"[pink3]folder '{folder_name}' created successfully.[/pink3]")
     except FileExistsError:
-        print(f"Folder '{folder_name}' already exists.")
+        console.print(f"[pink3]Folder '{folder_name}' already exists.[/pink3]")
 
 
 
@@ -30,9 +30,10 @@ def handle_deleted_user(username):
         temp_file_path = os.path.join(temp_folder, file)
         shutil.move(file_path, temp_file_path)
 
-    print (f"Successfully moved {len(files)} from {user_folder_path} to {temp_folder}.")
+    console.print (f"[orange3]Successfully moved {len(files)} from {user_folder_path} to {temp_folder}.[/orange3]")
 
 def sort_documents(folder_path):
+
     files = os.listdir(folder_path)
 
     for file in files:
@@ -48,7 +49,7 @@ def sort_documents(folder_path):
             destination_path = os.path.join(destination_folder, file)
             shutil.move(file_path, destination_path)
 
-    print(f"Documents sorted into appropriate folders in {folder_path}.")
+    console.print(f"[green4]Documents sorted into new file extension folders in {folder_path}.[/green4]")
 
 
 def parse_errors(log_file_path, target_directory):
@@ -72,7 +73,7 @@ def parse_errors(log_file_path, target_directory):
                     errors_log_file.write(line)
                 elif re.match(warning_regex, line):
                     warning_log_file.write(line)
-    print ("Log file parsed successfully")
+    console.print ("[deep_sky_blue1]Log file parsed successfully![/deep_sky_blue1]")
 
 
 def rename_files(directory, old_name, new_name):
@@ -85,7 +86,7 @@ def rename_files(directory, old_name, new_name):
             new_file_path = os.path.join(directory, new_name)
             os.rename(old_file_path, new_file_path)
             renamed_files += 1
-    print(f"Renamed {renamed_files} file(s) from {old_name} to {new_name}.")
+    console.print(f"[purple]Renamed {renamed_files} file(s) from {old_name} to {new_name}.[/purple]")
 
 def main():
     """Main function to run the CLI app
@@ -94,33 +95,45 @@ def main():
     """
 
     while True:
-        console.print("\n1. Create folder\n2. Delete user\n3.Sort documents\n4.Parse error logs\n5. Rename files")
-        choice = Prompt.ask("Choose a task(Enter the number)", choices=['1','2','3','4','5','6'], default='6')
+        console.print("\n[pink3]1. Create folder[/pink3]\n[orange3]2. Delete user[/orange3]\n[green4]3. Sort documents[/green4]\n[deep_sky_blue1]4. Parse error logs[/deep_sky_blue1]\n[purple]5. Rename files[/purple]")
+        choice = Prompt.ask("Choose a menu number. Selecting 6 will exit the program.)", choices=['1','2','3','4','5','6'])
 
         if choice =='1':
-            folder_name = Prompt.ask("Enter the folder name")
-            create_folder(folder_name)
+            folder_name = Prompt.ask("[pink3]Enter the folder name[/pink3]")
+            if os.path.exists(folder_name):
+                console.print("[red]Folder already exists. Please choose a different folder name[/red]")
+            else:
+                create_folder(folder_name)
         elif choice =='2':
-            username = Prompt.ask("Enter the username to handle the deleted user")
-            handle_deleted_user(username)
+            username = Prompt.ask("[orange3]Enter the username to handle the deleted user[/orange3]")
+            if not os.path.exists(username):
+                console.print("[red]Username does not exist in the file structure. Please try another username.[/red]")
+            else:
+                handle_deleted_user(username)
         elif choice == '3':
-            folder_path = Prompt.ask("Enter the folder path to sort documents")
-            sort_documents(folder_path)
+            folder_path = Prompt.ask("[green4]Enter the folder path to sort documents[/green4]")
+            if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+                console.print("[red]Invalid folder path. Please try again.[/red]")
+            else:
+                sort_documents(folder_path)
         elif choice == '4':
-            log_file_path = Prompt.ask("Enter the log file path")
-            target_directory = Prompt.ask("Enter the target directory to save the parsed logs")
-            parse_errors(log_file_path,target_directory)
+            log_file_path = Prompt.ask("[deep_sky_blue1]Enter the log file path[/deep_sky_blue1]")
+            target_directory = Prompt.ask("[deep_sky_blue1]Enter the target directory to save the parsed logs[/deep_sky_blue1]")
+            if not os.path.isfile(log_file_path) or not os.path.exists(target_directory) or not os.path.isdir(target_directory):
+                console.print("[red]Invalid log file path or target directory. Please try another entry.[/red]")
+            else:
+                parse_errors(log_file_path,target_directory)
         elif choice == '5':
-            directory_path = Prompt.ask("Enter the directory path to rename the files")
-            old_name = Prompt.ask("Enter the old file name")
-            new_name = Prompt.ask("Enter the new file name")
+            directory_path = Prompt.ask("[purple]Enter the directory path to rename the files:[/purple]")
+            old_name = Prompt.ask("[purple]Enter the old file name:[/purple]")
+            new_name = Prompt.ask("[purple]Enter the new file name:[/purple]")
             rename_files(directory_path, old_name, new_name)
 
         elif choice == '6':
             break
 
         else:
-            console.print("Invalid choice. Please try again.")
+            console.print("[red]Invalid choice. Please try again.[/red]")
 
 
 
